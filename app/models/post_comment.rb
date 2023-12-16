e890f64e40deb6e136fa8b3bd26e8683529a58a8
EC2 Default User <ec2-user@ip-172-31-35-128.ap-northeast-1.compute.internal>
2023-12-16 16:47:35 +0000

    コメント機能修正

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -2,6 +2,8 @@ class PostComment < ApplicationRecord
   belongs_to :customer
   belongs_to :post
 
+  validates :comment, presence: true
+
   #登録日が最新の会員が上に来るようにする指示
   default_scope -> { order(created_at: :desc) }
 end
